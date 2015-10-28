550b8bce7a447421129cb4437abb4eb4ab36f01a
Chris Hager <chris.hager@bitsworking.com>
2015-10-28 17:37:37 +0100

    start cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+from pdfx import cli
+import pytest
+
+curdir = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_cli():
+    parser = cli.create_parser()
+    parsed = parser.parse_args(['-j', 'pdfs/valid.pdf'])
+    assert parsed.json
+    assert parsed.pdf == "pdfs/valid.pdf"

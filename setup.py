c684284df7ef67a8fb6d5b565d4726f37053b91c
Chris Hager <chris.hager@bitsworking.com>
2015-10-29 08:57:57 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',
+    version='1.0.3',
 
-    description='Extract metadata and URLs from PDF files',
+    description='Extract metadata and URLs from PDF files, and download all referenced PDFs',
     long_description=long_description,
 
     # The project's main homepage.

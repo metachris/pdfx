f9c37cea25a4eae9d6ce8c2af7ee834384d90738
Chris Hager <chris.hager@bitsworking.com>
2015-10-31 23:24:40 +0100

    metadata

--- pdfx/backends.py
+++ pdfx/backends.py
@@ -24,6 +24,7 @@ from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
 from pdfminer.cmapdb import CMapDB
 from pdfminer.layout import LAParams
 from pdfminer.image import ImageWriter
+from pdfminer import psparser
 
 
 IS_PY2 = sys.version_info < (3, 0)
@@ -35,6 +36,8 @@ if not IS_PY2:
 def make_compat_str(in_str):
     """ Tries to guess encoding and return unicode """
     assert isinstance(in_str, (bytes, str, unicode))
+    if not in_str:
+        return in_str
     enc = chardet.detect(in_str)
     out_str = in_str.decode(enc['encoding'])
     if enc['encoding'] == "UTF-16BE":
@@ -64,13 +67,6 @@ class ReaderBackend(object):
     metadata = {}
     references = set()
 
-    def __init__(self, stream):
-        """
-        This is the only method required to implement. Should at least
-        extract `self.text` and `self.urls` from the stream.
-        """
-        pass
-
     def get_metadata(self):
         return self.metadata
 
@@ -93,7 +89,12 @@ class PDFMinerBackend(ReaderBackend):
         doc = PDFDocument(parser, password=password, caching=True)
         if doc.info:
             for k in doc.info[0]:
-                self.metadata[k] = make_compat_str(doc.info[0][k])
+                v = doc.info[0][k]
+                # print(repr(v), type(v))
+                if isinstance(v, (bytes, str, unicode)):
+                    self.metadata[k] = make_compat_str(v)
+                elif isinstance(v, (psparser.PSLiteral, psparser.PSKeyword)):
+                    self.metadata[k] = v.name
 
         # Extract Content
         text_io = BytesIO()
@@ -101,18 +102,22 @@ class PDFMinerBackend(ReaderBackend):
         converter = TextConverter(rsrcmgr, text_io, codec="utf-8",
                 laparams=LAParams(), imagewriter=None)
         interpreter = PDFPageInterpreter(rsrcmgr, converter)
+
+        self.metadata["Pages"] = 0
         for page in PDFPage.get_pages(self.pdf_stream, pagenos=pagenos,
                                       maxpages=maxpages, password=password,
                                       caching=True, check_extractable=True):
-            # Collect URL annotations
-            for annot in page.annots:
-                a = annot.resolve()
-                if "A" in a and "URI" in a["A"]:
-                    ref = Reference.from_url(a["A"]["URI"].decode("utf-8"))
-                    self.references.add(ref)
-
             # Read page contents
             interpreter.process_page(page)
+            self.metadata["Pages"] += 1
+
+            # Collect URL annotations
+            if page.annots:
+                for annot in page.annots:
+                    a = annot.resolve()
+                    if "A" in a and "URI" in a["A"]:
+                        ref = Reference.from_url(a["A"]["URI"].decode("utf-8"))
+                        self.references.add(ref)
 
         # Get text from stream
         self.text = text_io.getvalue().decode("utf-8")
@@ -124,21 +129,10 @@ class PDFMinerBackend(ReaderBackend):
         for url in urlmarker.get_urls(self.text):
             self.references.add(Reference.from_url(url))
 
-        # print(self.references)
-
-        # Get Metadata
-        # self.num_pages = self.pdf.getNumPages()
-        # self.pdf_metadata["Pages"] = self.num_pages
-        # doc_info = self.pdf.getDocumentInfo()
-        # if doc_info:
-        #     for k, v in doc_info.items():
-        #         if isinstance(v, PyPDF2.generic.IndirectObject):
-        #             self.pdf_metadata[k.strip("/")] = parse_str(v.getObject())
-        #         else:
-        #             self.pdf_metadata[k.strip("/")] = parse_str(v)
+        # TODO: Search for ArXiv References
 
 class TextBackend(ReaderBackend):
     def __init__(self, stream):
         self.text = stream.read()
-        self.urls_text = urlmarker.get_urls(self.text)
-        self.urls = self.urls_annotations | self.urls_text
+        for url in urlmarker.get_urls(self.text):
+            self.references.add(Reference.from_url(url))
